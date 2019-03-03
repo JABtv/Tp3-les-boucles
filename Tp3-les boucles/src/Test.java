59e9a905808e39949d6aca92c074cbfb89f55282
hello <hello@redmush>
2019-03-03 20:46:31 +0100

    Hello world

---
+++ Tp3-les boucles/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		for (int i=0; i<=100; i++) {
+			if (i==50) {
+				System.out.println("Moitié");
+			}
+			else System.out.println(i);
+			
+		}
+
+	}
+
+}
